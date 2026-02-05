f690f28b0f09d0d89bc8580477adfea47d8d692d
Oleksandr Bakai <saniabakay@gmail.com>
2026-02-05 02:11:13 +0100

    navbar 05.02.26

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,39 @@
 import {NavLink} from 'react-router-dom';
-import styles from'./Navbar.module.css';
+import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/about">About me</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
-      <NavLink to="/news">News</NavLink>
+      <NavLink
+        to="/"
+        className={({isActive}) => (isActive ? styles.active : styles.navText)}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/projects"
+        className={({isActive}) => (isActive ? styles.active : styles.navText)}
+      >
+        Projects
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({isActive}) => (isActive ? styles.active : styles.navText)}
+      >
+        About me
+      </NavLink>
+      <NavLink
+        to="/contacts"
+        className={({isActive}) => (isActive ? styles.active : styles.navText)}
+      >
+        Contacts
+      </NavLink>
+      <NavLink
+        to="/news"
+        className={({isActive}) => (isActive ? styles.active : styles.navText)}
+      >
+        News
+      </NavLink>
     </div>
   );
 }
--- src/pages/ContactInfo.jsx
+++ src/pages/ContactInfo.jsx
@@ -1,3 +1,26 @@
+import style from './Contacts.module.css';
 export default function ContactInfo() {
-  return <h1>Contacts</h1>;
+  return (
+    <div className={style.contactMainContainer}>
+      <div className={style.secondContainer}>
+        <div className={style.contactsHeader}>Contacts</div>
+
+        <p>
+          Email: <span>saniabakay@gmail.com</span>
+        </p>
+        <p>
+          Location: <span>Almere, Flevoland, The Netherlands</span>
+        </p>
+        <div className={style.contactButtons}>
+          <a
+            href="https://www.linkedin.com/in/oleksandr-bakai-9068a4389/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img alt="LinkedInLogo" src="LinkedIn.png" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
